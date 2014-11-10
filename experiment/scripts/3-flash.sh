929e7d840a31dab650c6be59b5f823eee91b756c
Adam Kučera <kucerad5@fit.cvut.cz>
2014-11-10 14:48:03 +0100

    Changes to experiment.

--- experiment/scripts/3-flash.sh
+++ experiment/scripts/3-flash.sh
@@ -1,7 +1,14 @@
 #! /bin/bash
+timestamp=$(date +%s)
+
+log="3-flash"
+log=$log$timestamp
+log=$log".log"
+
 for ((i = 0; i < $1; i++)); do
    options=("1" "10" "5" "50" "100")
    RANDOM=$[ $i + $[ RANDOM % 10 ]]
    selectedoption=${options[$RANDOM % ${#options[@]} ]}
-   java -jar ../../image_resizer_client/dist/image_resizer_client.jar -file ../images/image$selectedoption.zip -o $i.zip & >> 3-flash.log
+   java -jar ../../image_resizer_client/dist/image_resizer_client.jar -file ../images/image$selectedoption.zip -o $i.zip &
 done
+exit 0
